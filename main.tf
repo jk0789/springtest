b3fe200e0df4f645935c0006e3685443fe15faa8
jk <joshikarunakar07@gmail.com>
2023-07-27 19:04:27 +0530

    chnagereq

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
+
+  client_id       = "87b572d5-b79c-4ebf-a979-bcccaccb9440"
+  client_secret   = "nez8Q~kNWyRSBgAyuAjiYgQEOAwG8tCV80SibaWW"
+  tenant_id       = "2154e3a9-19a3-4870-8e95-08abb8f6d960"
+  subscription_id = "89f29eb9-3141-4ac6-8492-6213cd9a6822"
 }
 
 # Define the backend configuration for Azure Storage
@@ -25,11 +39,6 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     name       = "default"
     node_count = 2
     vm_size    = "Standard_DS2_v2"  # Change to your desired VM size
-  }
-
-  service_principal {
-    client_id     = "87b572d5-b79c-4ebf-a979-bcccaccb9440"  # Replace with your Azure Service Principal client_id
-    client_secret = "nez8Q~kNWyRSBgAyuAjiYgQEOAwG8tCV80SibaWW"  # Replace with your Azure Service Principal client_secret
   }
 
 }
